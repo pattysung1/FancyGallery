7b4338bdd79ad92b0837006ef59835d615011a1c
pattysung1 <a9870456321@gmail.com>
2023-12-01 20:51:51 -0500

    Map zoom size

--- app/src/main/java/edu/vt/cs5254/fancygallery/MapFragment.kt
+++ app/src/main/java/edu/vt/cs5254/fancygallery/MapFragment.kt
@@ -5,7 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.preference.PreferenceManager
 import edu.vt.cs5254.fancygallery.databinding.FragmentMapBinding
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.views.CustomZoomButtonsController
+import org.osmdroid.views.MapView
 
 class MapFragment: Fragment() {
 
@@ -13,6 +19,7 @@ class MapFragment: Fragment() {
     private val binding
         get() = checkNotNull(_binding) {"FragmentMapBinding null"}
 
+    private val vm: MapViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,8 +29,52 @@ class MapFragment: Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(Configuration.getInstance()){
+            load(
+                context,
+                PreferenceManager.getDefaultSharedPreferences(requireContext())
+            )
+            userAgentValue = requireContext().packageName
+        }
+
+        binding.mapView.apply {
+            minZoomLevel = 1.5
+            maxZoomLevel = 15.0
+            setTileSource(TileSourceFactory.MAPNIK)
+            isVerticalMapRepetitionEnabled = false
+            setScrollableAreaLimitLatitude(
+                MapView.getTileSystem().maxLatitude,
+                MapView.getTileSystem().minLatitude,
+                0
+            )
+            isTilesScaledToDpi = true
+            zoomController.setVisibility(
+                CustomZoomButtonsController.Visibility.ALWAYS
+            )
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onPause() {
+        super.onPause()
+        with(binding.mapView){
+            vm.saveMapState(zoomLevelDouble, mapCenter)
+            onPause()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        with(binding.mapView){
+            onResume()
+            controller.setZoom(vm.zoomLevel)
+            controller.setCenter(vm.mapCenter)
+        }
     }
 }
